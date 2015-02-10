357750bce28fc8fb3ef7d4f691c4e610202d9ed2
Thibault Milan <hello@thibaultmilan.com>
2015-02-10 23:07:37 +0100

    remove the template auto-regeneration

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,7 @@ gulp.task('extras', function () {
 
 gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 
-gulp.task('connect', ['styles','templates'], function () {
+gulp.task('connect', ['styles'], function () {
   var serveStatic = require('serve-static');
   var serveIndex = require('serve-index');
   var app = require('connect')()
